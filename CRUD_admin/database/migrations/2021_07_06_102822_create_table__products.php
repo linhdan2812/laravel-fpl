743e145b4f52f071ef88c764fbc5a13f464f2498
linhdan2812 <linhdanzyl2812@gmail.com>
2021-07-06 20:12:19 +0700

    add crud

---
+++ CRUD_admin/database/migrations/2021_07_06_102822_create_table__products.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cate_id');
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->integer('price')->default(0);
+            $table->text('detail');
+            $table->integer('sale')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Products');
+    }
+}
